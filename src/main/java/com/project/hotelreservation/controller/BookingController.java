a8df478ea59105a5b6d360f736a73745a42469af
nansun <nansun768@gmail.com>
2023-11-03 04:44:48 +0000

    booking function is working now

--- src/main/java/com/project/hotelreservation/controller/BookingController.java
+++ src/main/java/com/project/hotelreservation/controller/BookingController.java
@@ -1,26 +1,31 @@
 package com.project.hotelreservation.controller;
 
+import com.project.hotelreservation.model.entity.AdditionalServices;
 import com.project.hotelreservation.model.entity.Booking;
-import com.project.hotelreservation.model.entity.Payment;
+import com.project.hotelreservation.model.entity.Customer;
+import com.project.hotelreservation.model.entity.Room;
+import com.project.hotelreservation.service.AdditionalServicesService;
+import com.project.hotelreservation.service.BookingService;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import com.project.hotelreservation.repository.BookingRepository;
-import com.project.hotelreservation.repository.PaymentRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
 
 
 @Controller
 @AllArgsConstructor
 public class BookingController {
-    private BookingRepository bookingRepository;
-    private PaymentRepository paymentRepository;
+    private final BookingService bookingService;
+    private final AdditionalServicesService additionalServicesService;
 
     @GetMapping("/booking")
     public String showBookingPage(Model model) {
-
         // Create a new Booking and set the initial state
         Booking booking = new Booking();
 
@@ -33,20 +38,33 @@ public class BookingController {
         return "customer/booking";
     }
 
-    @PostMapping("/booking")
-    public String processBooking(@ModelAttribute("newBooking") Booking newBooking) {
-        Booking savedBooking = bookingRepository.save(newBooking);
+    @PostMapping("/proceed-booking")
+    public String processBooking() {
+        // proceed to the additional services page
+        return "redirect:/additional-services";
+    }
 
-        // Create a Payment object
-        Payment payment = new Payment();
-        payment.setAmount(100.0); // Set the payment amount as needed
-        payment.setBooking(savedBooking); // Associate the payment with the booking
+    @PostMapping("/confirm-booking")
+    public String confirmBooking(@RequestParam(required = false) List<Integer> serviceIds,
+                                 Model model,
+                                 HttpSession session) {
+        List<AdditionalServices> selectedServices = additionalServicesService.getServicesByIds(serviceIds);
+        model.addAttribute("selectedServices",selectedServices);
+        session.setAttribute("selectedServices",selectedServices);
+        return "customer/confirmation";
+    }
 
-        paymentRepository.save(payment);
-
-        // Redirect to the payment page
-        return "redirect:/payment/" + savedBooking.getBookingId();
-
+    @PostMapping("/save-booking")
+    public String saveBooking(HttpSession session) {
+        Room room = (Room) session.getAttribute("room");
+        Customer customer = (Customer) session.getAttribute("customer");
+        @SuppressWarnings("unchecked")
+        List<AdditionalServices> selectedServices = (List<AdditionalServices>) session.getAttribute("selectedServices");
+        Date checkInDate = (Date) session.getAttribute("checkInDate");
+        Date checkOutDate = (Date) session.getAttribute("checkOutDate");
+        bookingService.save(room, selectedServices, customer, checkInDate, checkOutDate);
+        // proceed to the payment page
+        return "redirect:/payment";
     }
 
     @GetMapping("/viewOrders")
---
+++ src/main/java/com/project/hotelreservation/service/serviceImpl/BookingServiceImpl.java
@@ -0,0 +1,38 @@
+package com.project.hotelreservation.service.serviceImpl;
+
+import com.project.hotelreservation.enums.BookingStatus;
+import com.project.hotelreservation.model.entity.AdditionalServices;
+import com.project.hotelreservation.model.entity.Booking;
+import com.project.hotelreservation.model.entity.Customer;
+import com.project.hotelreservation.model.entity.Room;
+import com.project.hotelreservation.repository.BookingRepository;
+import com.project.hotelreservation.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookingServiceImpl implements BookingService {
+    private final BookingRepository bookingRepository;
+
+    @Override
+    public void save(Room room, List<AdditionalServices> services, Customer customer, Date checkInDate, Date checkOutDate) {
+        Booking booking = new Booking();
+        booking.setRoom(room);
+        booking.setCustomer(customer);
+        booking.setAdditionalServices(services);
+        booking.setCheckInDate(checkInDate);
+        booking.setCheckOutDate(checkOutDate);
+        booking.setPayment(null);
+        LocalDate localDate = LocalDate.now();
+        Date currentDate = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        booking.setBookingDate(currentDate);
+        booking.setStatus(BookingStatus.PENDING);
+        bookingRepository.save(booking);
+    }
+}
